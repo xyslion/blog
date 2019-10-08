79a3934fa266d1731926f96a15a3ddb46d45ff6a
ys.x <ysx@ys.x-Lenovo-IdeaPad-Yoga-13>
2019-10-08 20:41:04 +0800

    theme -> even

---
+++ script/release.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+if [[ -d public ]]; then
+    GLOBIGNORE=*.git
+    rm -rf -v public/*
+fi
+
+hugo
+
+if [[ -n "$1" ]]; then
+    cd public
+    git add -A
+    git commit -m "$1"
+    git push
+else
+    echo
+    echo "[WARN] Files will NOT be uploaded to Github without adding comments."
+    echo "Usage: $(basename "$0") COMMIT_COMMENTS"
+fi
