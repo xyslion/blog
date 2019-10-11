54d1a176bc05c4a26eb72237433e91c63fca0709
ys.x <ysx@ys.x-Lenovo-IdeaPad-Yoga-13>
2019-10-11 17:44:53 +0800

    add goroutine_pool.md, two_nums_mul.md

--- script/release.sh
+++ script/release.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+rm -rf public
+
+git clone "git@github.com:xyslion/xyslion.github.io.git" public
+
 if [[ -d public ]]; then
     GLOBIGNORE=*.git
     rm -rf -v public/*
@@ -7,15 +11,10 @@ fi
 
 hugo
 
-if [[ -n "$1" ]]; then
-    cd public
-    git remote add origin "git@github.com:xyslion/xyslion.github.io.git"
-    git add -A
-    git commit -m "$1"
-    git pull origin master
-    git push -u origin master
-else
-    echo
-    echo "[WARN] Files will NOT be uploaded to Github without adding comments."
-    echo "Usage: $(basename "$0") COMMIT_COMMENTS"
-fi
+comment="${1:-'update blog'}"
+echo $comment
+
+cd public
+git add -A
+git commit -m "${comment}"
+git push
